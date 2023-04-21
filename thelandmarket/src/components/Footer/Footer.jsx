e4a8126836541385155f2f1d1fa0a3e443ce33b8
TahaKhan1099 <85824973+TahaKhan1099@users.noreply.github.com>
2023-04-21 07:08:12 +0500

    DoneWithFooterUI

--- thelandmarket/src/components/Footer/Footer.jsx
+++ thelandmarket/src/components/Footer/Footer.jsx
@@ -1,91 +1,89 @@
 import {
   Typography,
-  Grid,
   Box,
-  createTheme,
-  ThemeProvider,
   Container,
   Button,
   Card,
   CardContent,
   CardMedia,
+  CssBaseline,
+  OutlinedInput,
+  Link,
 } from "@mui/material";
-import Paper from "@mui/material/Paper";
-import Stack from "@mui/material/Stack";
-import { styled } from "@mui/material/styles";
+import { UilMessage, UilFacebookF, UilTwitter, UilInstagram } from "@iconscout/react-unicons";
 
 import React from "react";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+function CopyRight() {
+  return (
+    <Typography variant="body2" color="text.secondary" textAlign="center">
+      {"CopyRight © "}
+      <Link color="inherit" href="https://google.com/">
+        thelandmarket.pk
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 const Footer = () => {
   return (
     <Box
       sx={{
-        marginTop: "5rem",
+        marginTop: "15rem",
         borderTop: "2px solid #A6D0DD",
         display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
+        flexDirection: "column",
+        minHeight: "20vh",
+        backgroundColor: "#8df",
       }}
     >
-      {/* <Box sx={{ borderTop: "2px solid #A6D0DD", marginTop: "10rem" }}>
-       
-      </Box> */}
-      {/* <Stack
-        sx={{ marginTop: "2rem" }}
-        direction={{ xs: "column", sm: "row" }}
-        spacing={{ xs: 10, sm: 20, md: 80 }}
+      <CssBaseline />
+      <Container
+        component="main"
+        sx={{ mt: 2, mb: 8, textAlign: "center" }}
+        maxWidth="sm"
       >
-        <Item sx={{border:"none", boxShadow:"none"}}>
-          <Card sx={{border:"none", boxShadow: "none"}}>
-            <CardMedia sx={{width:"30%"}} component="img" image="/src/assets/images/logoNew.PNG"/>
-          </Card>
-        </Item>
-        <Item>Item 2</Item>
-        <Item>Item 3</Item>
-      </Stack> */}
-
-      <Box  sx={{ backgroundColor: "#cef", width: "100vh", bottom: "0"}}>
-        <div
-          className="query-container"
-          style={{
-            width: "100%",
-            padding: "2rem",
-            backgroundColor: "#cef",
-            gap: "1rem",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <span>Queries</span>
-          <div
-            className="queries"
-            style={{
-              backgroundColor: "#fff",
-              padding: "0 10px",
-              borderRadius: "35px",
-              alignItems: "center",
-            }}
-          >
-            <input
-              type="text"
-              placeholder="Query"
-              style={{
-                backgroundColor: "transparent",
-                border: "none",
-                outline: "none",
-                padding: "1rem",
-              }}
-            />
-          </div>
-        </div>
+        <Typography variant="h5" mt={5} component="h2" gutterBottom>
+          Connect With Us
+        </Typography>
+        <Box mt={3} textAlign="center">
+          <Button href="https://facebook.com/">
+            <UilFacebookF size="1rem" color="#000" />
+          </Button>
+          <Button href="https://twitter.com/">
+            <UilTwitter size="1rem" color="#000" />
+          </Button>
+          <Button href="https://instagram.com/">
+            <UilInstagram size="1rem" color="#000" />
+          </Button>
+        </Box>
+        <Typography variant="body1" mt={7}>Send Us Your Queries</Typography>
+        <Box mt={2} ml={7} textAlign="center" display="inline-flex">
+          <OutlinedInput
+            sx={{ backgroundColor: "#ffffff", borderRadius: "2rem", marginLeft:"1rem"}}
+          />
+          <Button sx={{ padding: "15.5px 1px", marginLeft:"0.5rem" ,transform:"rotate(-30deg)", "&:hover":{backgroundColor: "#8df"} }}>
+            <UilMessage size="2rem" color="#3A98B9" />
+          </Button>
+        </Box>
+      </Container>
+      <Box
+        component="footer"
+        sx={{
+          py: 1,
+          px: 1,
+          mt: "auto",
+          backgroundColor: "#3A98B9",
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography variant="body1" textAlign="center">
+            TLM
+          </Typography>
+          <CopyRight />
+        </Container>
       </Box>
     </Box>
   );
--- thelandmarket/src/components/Home/Home.jsx
+++ thelandmarket/src/components/Home/Home.jsx
@@ -7,6 +7,7 @@ import {
   Grid,
   Box,
   createTheme,
+  OutlinedInput,
   ThemeProvider,
   Container,
   Button,
