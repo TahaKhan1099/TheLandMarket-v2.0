4398119a954b8335a1733e014dfab991383ece2a
TahaKhan1099 <85824973+TahaKhan1099@users.noreply.github.com>
2023-05-05 23:45:34 +0500

    working11

--- thelandmarket/src/components/DealerDashboard/DealerDashboard.jsx
+++ thelandmarket/src/components/DealerDashboard/DealerDashboard.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { UserAuth } from "../../Context/AuthContext";
+import { Container, Box, Button, Typography } from "@mui/material";
 
 const DealerDashboard = () => {
-  return (
-    <div>DealerDashboard</div>
-  )
-}
+  const navigate = useNavigate();
+  const { user, logout } = UserAuth();
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/home");
+      window.alert("You are logged out");
+      console.log("You are logged out");
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
 
-export default DealerDashboard
+  return (
+    <Container maxWidth="xl">
+      <Box mt={20}>
+        <Typography> User Email: {user && user.email}</Typography>
+        <Button onClick={handleLogout}>LogOut</Button>
+      </Box>
+    </Container>
+  );
+};
+
+export default DealerDashboard;
--- thelandmarket/src/components/DealerRegDetails/DealerRegDetails.jsx
+++ thelandmarket/src/components/DealerRegDetails/DealerRegDetails.jsx
@@ -9,13 +9,99 @@ import {
   Container,
   Button,
 } from "@mui/material";
-import GoogleIcon from "@mui/icons-material/Google";
 import background from "../../assets/images/registration1.jpg";
-import { UserAuth } from "../../Context/AuthContext";
+import { auth } from "../../firebase";
 
 import { useNavigate } from "react-router-dom";
 
 const DealerRegDetails = () => {
+  const navigate = useNavigate();
+  const [dealerData, setDealerData] = useState({
+    name: "",
+    age: "",
+    phoneNumber: "",
+    CNIC: "",
+    societyName: "",
+    agencyName: "",
+    officeAddress: "",
+    issuedEmployeeID: "",
+    officePhoneNumber: "",
+  });
+
+  let name, value;
+  const postDealerData = (event) =>{
+    name = event.target.name;
+    value = event.target.value;
+    setDealerData({...dealerData, [name]: value}); //accessing dynamically
+  };
+
+  //connecting with Firebase
+  const submitDealerData = async (event) =>{
+    event.preventDefault();
+    const {
+      name,
+			age,
+			phoneNumber,
+			CNIC,
+			societyName,
+			agencyName,
+			officeAddress,
+			issuedEmployeeID,
+			officePhoneNumber,
+    } = dealerData;
+
+    if(
+      name &&
+			age &&
+			phoneNumber &&
+			CNIC &&
+			societyName &&
+			agencyName &&
+			officeAddress &&
+			issuedEmployeeID &&
+			officePhoneNumber
+    ) {
+      const res = await fetch(
+        "https://tlm-auth-development-default-rtdb.firebaseio.com/DealerDataRecords.json",
+        {
+          method: "POST",
+          headers:{"content-type": "application/json",},
+          body: JSON.stringify({
+            name,
+						age,
+						phoneNumber,
+						CNIC,
+						societyName,
+						agencyName,
+						officeAddress,
+						issuedEmployeeID,
+						officePhoneNumber,
+						userId: userId,
+          }), //making sure the data passed is in string
+        }
+      );
+      if(res){
+        setDealerData({
+          name: "",
+					age: "",
+					phoneNumber: "",
+					CNIC: "",
+					societyName: "",
+					agencyName: "",
+					officeAddress: "",
+					issuedEmployeeID: "",
+					officePhoneNumber: "",
+        });
+        window.alert("Registered");
+        navigate("/dealerDashboard");
+      }
+      else{window.alert("Form Not Filled Completely");}
+    }
+    else{window.alert("Form Not Filled Completely");}
+  };
+  const currentUser = auth.currentUser;
+  const userId = currentUser ? currentUser.uid : null;
+
   return (
     <Container maxWidth="xl">
       <Box sx={{ position: "relative" }}>
@@ -48,13 +134,13 @@ const DealerRegDetails = () => {
             marginTop: "3%",
             borderRadius: "1rem",
             width: "45%",
-            height: '40rem'
+            height: "40rem",
           },
           "@media(max-width: 600px)": {
             marginTop: "3%",
             borderRadius: "1rem",
             width: "45%",
-            height: '40rem'
+            height: "40rem",
           },
         }}
       >
@@ -66,10 +152,10 @@ const DealerRegDetails = () => {
             marginTop: "2rem",
             fontFamily: "Poppins",
             "@media(max-width: 800px)": {
-              fontSize: '1.5rem'
+              fontSize: "1.5rem",
             },
             "@media(max-width: 600px)": {
-              fontSize: '1rem'
+              fontSize: "1rem",
             },
           }}
         >
@@ -84,7 +170,7 @@ const DealerRegDetails = () => {
             },
           }}
         >
-          <form>
+          <form method="POST">
             <Box sx={{ marginTop: "2rem" }}>
               <Typography
                 sx={{
@@ -102,30 +188,33 @@ const DealerRegDetails = () => {
                 type="text"
                 name="name"
                 required
+                value={dealerData.name}
+                onChange={postDealerData}
                 placeholder="Name"
                 style={{
                   border: "1px solid #c0c0c0",
                   borderRadius: "4px",
-                  marginTop: '0.5rem',
+                  marginTop: "0.5rem",
                   width: "280px",
                   height: "30px",
                   boxSizing: "border-box",
                   textAlign: "center",
-                
                 }}
               />
             </Box>
 
             <Box sx={{ marginTop: "0.2rem" }}>
-            <input
+              <input
                 type="number"
                 name="age"
+                value={dealerData.age}
+							onChange={postDealerData}
                 required
                 placeholder="Age"
                 style={{
                   border: "1px solid #c0c0c0",
                   borderRadius: "4px",
-                  marginTop: '0.5rem',
+                  marginTop: "0.5rem",
                   width: "280px",
                   height: "30px",
                   boxSizing: "border-box",
@@ -134,15 +223,17 @@ const DealerRegDetails = () => {
               />
             </Box>
             <Box sx={{ marginTop: "0.2rem" }}>
-            <input
+              <input
                 type="number"
                 name="phoneNumber"
+                value={dealerData.phoneNumber}
+							onChange={postDealerData}
                 required
                 placeholder="Phone Number"
                 style={{
                   border: "1px solid #c0c0c0",
                   borderRadius: "4px",
-                  marginTop: '0.5rem',
+                  marginTop: "0.5rem",
                   width: "280px",
                   height: "30px",
                   boxSizing: "border-box",
@@ -151,15 +242,17 @@ const DealerRegDetails = () => {
               />
             </Box>
             <Box sx={{ marginTop: "0.2rem" }}>
-            <input
+              <input
                 type="number"
                 name="CNIC"
+                value={dealerData.CNIC}
+							onChange={postDealerData}
                 required
                 placeholder="CNIC"
                 style={{
                   border: "1px solid #c0c0c0",
                   borderRadius: "4px",
-                  marginTop: '0.5rem',
+                  marginTop: "0.5rem",
                   width: "280px",
                   height: "30px",
                   boxSizing: "border-box",
@@ -183,12 +276,14 @@ const DealerRegDetails = () => {
               <input
                 type="text"
                 name="societyName"
+                value={dealerData.societyName}
+							onChange={postDealerData}
                 required
                 placeholder="Society Name"
                 style={{
                   border: "1px solid #c0c0c0",
                   borderRadius: "4px",
-                  marginTop: '0.5rem',
+                  marginTop: "0.5rem",
                   width: "280px",
                   height: "30px",
                   boxSizing: "border-box",
@@ -197,15 +292,17 @@ const DealerRegDetails = () => {
               />
             </Box>
             <Box sx={{ marginTop: "0.2rem" }}>
-            <input
+              <input
                 type="text"
                 name="agencyName"
+                value={dealerData.agencyName}
+							onChange={postDealerData}
                 required
                 placeholder="Agency Name"
                 style={{
                   border: "1px solid #c0c0c0",
                   borderRadius: "4px",
-                  marginTop: '0.5rem',
+                  marginTop: "0.5rem",
                   width: "280px",
                   height: "30px",
                   boxSizing: "border-box",
@@ -214,15 +311,17 @@ const DealerRegDetails = () => {
               />
             </Box>
             <Box sx={{ marginTop: "0.2rem" }}>
-            <input
+              <input
                 type="text"
                 name="officeAddress"
+                value={dealerData.officeAddress}
+							onChange={postDealerData}
                 required
                 placeholder="Office Address"
                 style={{
                   border: "1px solid #c0c0c0",
                   borderRadius: "4px",
-                  marginTop: '0.5rem',
+                  marginTop: "0.5rem",
                   width: "280px",
                   height: "30px",
                   boxSizing: "border-box",
@@ -231,15 +330,17 @@ const DealerRegDetails = () => {
               />
             </Box>
             <Box sx={{ marginTop: "0.2rem" }}>
-            <input
+              <input
                 type="text"
                 name="issuedEmployeeID"
+                value={dealerData.issuedEmployeeID}
+							onChange={postDealerData}
                 required
                 placeholder="Office Issued Employee ID"
                 style={{
                   border: "1px solid #c0c0c0",
                   borderRadius: "4px",
-                  marginTop: '0.5rem',
+                  marginTop: "0.5rem",
                   width: "280px",
                   height: "30px",
                   boxSizing: "border-box",
@@ -248,15 +349,17 @@ const DealerRegDetails = () => {
               />
             </Box>
             <Box sx={{ marginTop: "0.2rem" }}>
-            <input
+              <input
                 type="text"
                 name="officePhoneNumber"
+                value={dealerData.officePhoneNumber}
+							onChange={postDealerData}
                 required
                 placeholder="Office Phone Number"
                 style={{
                   border: "1px solid #c0c0c0",
                   borderRadius: "4px",
-                  marginTop: '0.5rem',
+                  marginTop: "0.5rem",
                   width: "280px",
                   height: "30px",
                   boxSizing: "border-box",
@@ -275,12 +378,12 @@ const DealerRegDetails = () => {
                 "&:hover": { color: "#3A98B9", backgroundColor: "#ffffff" },
               }}
               type="submit"
+              onClick={submitDealerData}
             >
               SUBMIT
             </Button>
           </form>
         </Box>
-        
       </Box>
     </Container>
   );
